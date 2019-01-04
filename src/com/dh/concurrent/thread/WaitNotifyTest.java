a24f6fd8496e954b1b2e1974f5dbdc42a7106234
qe2592008 <qe259@163.com>
2019-01-04 18:36:26 +0800

    wait/notifyAll example

--- src/com/dh/concurrent/thread/WaitNotifyTest.java
+++ src/com/dh/concurrent/thread/WaitNotifyTest.java
@@ -2,6 +2,7 @@ package com.dh.concurrent.thread;
 
 /**
  * wait和notify/notifyAll方法都是在Object类中定义的，那么每个类都可以使用这三个方法
+ * 注意，这三个操作都要与锁相关联，也就是说都需要获取到对象的锁之后才能操作
  */
 public class WaitNotifyTest {
 
@@ -11,7 +12,9 @@ public class WaitNotifyTest {
 
     public static void main(String[] args) {
         Runnable runnable = () -> {
+            System.out.println(Thread.currentThread().getState());
             synchronized (lock) {
+                System.out.println(Thread.currentThread().getState());
                 while (flag == 0) {
                     try {
                         System.out.println(Thread.currentThread().getName() + "-----等着吧");
@@ -27,16 +30,18 @@ public class WaitNotifyTest {
             synchronized (lock) {
                 try {
                     System.out.println(Thread.currentThread().getName() + "----睡一会吧");
-                    Thread.sleep(2000);
+                    Thread.sleep(50);
                     System.out.println(Thread.currentThread().getName() + "----醒过来了");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 flag = 1;
                 System.out.println(flag);
-                lock.notifyAll();
+//                lock.notifyAll();
+                lock.notify();
             }
         });
+        // 连开10个线程
         for (int i = 0;i < 10;i++) {
             Thread t = new Thread(runnable);
             t.setName("THREAD--" + i);
